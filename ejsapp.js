cad853c7d931d37825e5d2da200e7aeaee2061c0
Tim Yu <chonghao.yu@gmail.com>
2023-07-18 14:34:02 +0800

    feature: 1.EJS 基本 用法測試完成

--- ejsapp.js
+++ ejsapp.js
@@ -9,10 +9,29 @@ app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
     // res.render("index.ejs");
-    res.render("index");
+    let ejs_3 = "<h1>EJS3</h1>";
+    let ejs_2 = [
+        { name: "Java", rating: 10 },
+        { name: "Kotlin", rating: 10 },
+        { name: "Rust", rating: 10 },
+        { name: "JS", rating: 10 }
+    ];
+    res.render("index", { ejs_2, ejs_3 });
 });
 
-app.get("/:who", (req, res) => {
+app.get("/language", (req, res) => {
+    let { count } = req.params;
+    let countN = Number(count);
+    let code_language = [
+        { name: "Java", rating: 10 },
+        { name: "Kotlin", rating: 10 },
+        { name: "Rust", rating: 10 },
+        { name: "JS", rating: 10 }
+    ]
+    res.render("index", { countData: countN, code_language });
+});
+
+app.get("/person/:who", (req, res) => {
     let { who } = req.params;
 
     let code_language = [
